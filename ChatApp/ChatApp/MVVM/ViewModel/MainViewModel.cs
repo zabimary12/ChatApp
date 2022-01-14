439002d19ce4197414304600495b69055b5e70a0
zabimary12 <60661185+zabimary12@users.noreply.github.com>
2022-01-15 01:02:59 +0200

    Add files via upload

---
+++ ChatApp/ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.MVVM.Model;
+using ChatClient.Net;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+        public RelayCommand ConnectToServerCommand { get; set; }
+        public RelayCommand SentMessageCommand { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        private Server _server;
+
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectEvent += UserConnected;
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(UserName), o => !string.IsNullOrEmpty(UserName));
+
+            SentMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage()
+            };
+
+            if(!Users.Any(x => x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+        }
+    }
+}
---
+++ ChatApp/ChatServer/Client.cs
@@ -0,0 +1,58 @@
+﻿using ChatServer.Net.IO;
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class Client
+    {
+        public string UserName { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+        PacketReader _packetReader;
+
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+            _packetReader = new PacketReader(ClientSocket.GetStream());
+
+            var opcode = _packetReader.ReadByte();
+            UserName = _packetReader.ReadMessage();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {UserName}");
+
+            Task.Run(() => Process());
+        }
+
+        void Process()
+        {
+            while(true)
+            {
+                try
+                {
+                    var opcode = _packetReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 5:
+                            var msg = _packetReader.ReadMessage();
+                            Console.WriteLine($"[{DateTime.Now}]: Message received! {msg}");
+                            Program.BroadcastMessage($"[{DateTime.Now}]: [{UserName}]: {msg}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine($"[{UID.ToString()}]: Disconnected!");
+                    Program.BroadcastDisconnect(UID.ToString());
+                    ClientSocket.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
